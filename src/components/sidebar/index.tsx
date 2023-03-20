f395f1064894f01d21dcfd37f021dc0c2ce3ba42
O2hope <agusvaldes2@live.com>
2023-03-19 21:23:24 -0600

    Fix styles and Add history menu

--- src/components/sidebar/index.tsx
+++ src/components/sidebar/index.tsx
@@ -1,19 +1,17 @@
 import { component$ } from "@builder.io/qwik";
 import SidebarItem from "@/components/sidebar-item";
-import {GoHubot} from "@qwikest/icons/octicons"
+import {LuBot, LuHistory} from "@qwikest/icons/lucide"
 export const Sidebar = component$(() => {
   return (
-    <div class={"flex-col gap-2 flex bg-base-200 h-full z-20 fixed  w-[260px] "}>
-      <div class="flex gap-2 items-center px-4 min-h-16 ">
+    <div class={"flex-col gap-2 flex bg-base-200 h-full z-20 fixed  w-[260px] py-2"}>
+      <div class="btn btn-ghost normal-case text-xl flex-none mx-4 ">
         <img src="/images/logo.svg" alt="Logo" height={48} width={48}></img>
-        <div class="text-3xl">
-          <span class="text-primary">Mi</span>mercado
-
-        </div>
+          Mi<span class="text-primary">mercado</span>
 
       </div>
-        <ul class="prose menu px-2 prose-a:no-underline prose-a:p-2">
-        <SidebarItem route="/" icon={GoHubot} name="Price generator" />
+        <ul class=" menu px-2 prose-a:no-underline mx-2 pt-2">
+        <SidebarItem route="/" icon={LuBot} name="Price generator" />
+          <SidebarItem route="/history" icon={LuHistory} name="History" />
         </ul>
     </div>
   );
---
+++ src/routes/history/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from "@builder.io/qwik";
+
+export default component$(() => {
+
+  return(
+    <div class="prose w-full max-w-5xl flex-grow">
+      <h1>History</h1>
+    </div>
+    )
+})
